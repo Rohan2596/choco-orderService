aa57b92cbfca231b10156c1518c9f728bb093184
Rohan2596 <rohankadam662@gmail.com>
2020-12-17 12:23:25 +0530

    ADDED:Http-errors and order.controller.js .

---
+++ controller/order.controller.js
@@ -0,0 +1,17 @@
+class OrderController{
+
+    placeOrder=(req,res,next)=>{
+        
+    };
+    editOrder=(req,res,next)=>{
+
+    };
+    getAOrderofCustomer=(req,res,next)=>{
+
+    };
+    getAllOrderOfCustomer=(req,res,next)=>{
+
+    };
+    
+}
+module.exports=new OrderController();
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express=require('express')
 const app=express();
 const routes=require('./routes/route')
+const createError = require('http-errors')
 
 app.use("/choco/customer/order",routes);
 
+
+app.use((req, res, next) => {
+    next(createError(404));
+})
+
+app.use((error, req, res, next) => {
+    let response = {
+        success: false,
+        status: 500,
+        message: error.message
+    };
+    res.json(response);
+})
 
 
 app.listen(3001,()=>{
     console.log("Listening to port 3001 for Ordering Service.");
 })
+
